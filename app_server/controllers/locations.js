b77fc72c8375a2e47ff7f6e3b21b898b89923ee1
Hector Perez M <hector.perez.m@usach.cl>
2019-07-20 14:41:38 -0400

    Se crearon vistas dinámicas, controladores pasan ahora la información

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,16 +1,92 @@
 /* GET home page */
 const homelist = (req, res) => {
-  res.render('locations-list', {title: 'Home'});
+  res.render('locations-list', 
+  {
+    title: 'Loc8r - find a place to work with wifi',
+    pageHeader: {
+      title: 'Loc8r',
+      strapline: 'Fin places to work with wifi near you!'
+    },
+    locations: [
+      {
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '100m'
+      },
+      {
+        name: 'MarleiCofi',
+        address: '300 High Street, Reading, RG6 1PS',
+        rating: 4,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '150m'
+      },
+      {
+        name: 'Burger Queen',
+        address: '125 Down Street, Lecturer, RG6 1PS',
+        rating: 4,
+        facilities: ['Food', 'Premium wifi'],
+        distance: '90m'
+      }
+    ]
+  });
 };
 
 /* GET Location info page */
 const locationInfo = (req, res) => {
-  res.render('location-info', {title: 'Location Info'});
+  res.render('location-info', 
+  {
+    title: 'Starcups',
+    pageHeader: {title: 'Starcups'},
+    sidebar: {
+      context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+      callToAction: 'If you\'ve been and you like it - or if you don\'t - please leavea review to help other people just like you.'
+    },
+    location: {
+      name: 'Starcups',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 3,
+      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+      coords: {lat: 51.455041, lng: -0.9690884},
+      openingTimes: [{
+        days: 'Monday - Friday',
+        opening: '7:00am',
+        closing: '7:00pm',
+        closed: false
+      }, {
+        days: 'Saturday',
+        opening: '8:00am',
+        closing: '5:00pm',
+        closed: false
+      }, {
+        days: 'Sunday',
+        closed: true
+      }],
+      reviews: [{
+        author: 'Sherlock Holmes',
+        rating: 5,
+        timestamp: '16 July 2010',
+        reviewText: 'What a great place!'
+      }, {
+        author: 'Homer Simpson',
+        rating: 3,
+        timestamp: '16 July 2011',
+        reviewText: 'Do\'h'
+      }]
+    }
+  });
 };
 
 /* GET Add review page */
 const addReview = (req, res) => {
-  res.render('location-review-form', {title: 'Add Review page'});
+  res.render('location-review-form', 
+  {
+    title: 'Review Starcups on Loc8r',
+    pageHeader: {
+      title: 'Review Starcups'
+    }
+  });
 };
 
 module.exports = {
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,5 +1,8 @@
 const about = (req, res) => {
-  res.render('generic-text', {title: 'About page'});
+  res.render('generic-text', {
+    title: 'About page',
+    content: 'Loc8r was created to help people find places to sit down and get a bit of work done.<br/></br> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit sint, repellendus dignissimos eligendi quidem nesciunt nisi alias laborum minus adipisci sed facilis unde consectetur culpa illum sunt velit tenetur soluta. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit sint, repellendus dignissimos eligendi quidem nesciunt nisi alias laborum minus adipisci sed facilis unde consectetur culpa illum sunt velit tenetur soluta. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit sint, repellendus dignissimos eligendi quidem nesciunt nisi alias laborum minus adipisci sed facilis unde consectetur culpa illum sunt velit tenetur soluta. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit sint, repellendus dignissimos eligendi quidem nesciunt nisi alias laborum minus adipisci sed facilis unde consectetur culpa illum sunt velit tenetur soluta.'
+  });
 };
 
 module.exports = {
