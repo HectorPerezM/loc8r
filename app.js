819cda4393edd1d618459f20e00b58fc3e69e267
Hector Perez M <hector.perez.m@usach.cl>
2019-07-20 18:58:14 -0400

    Añadido modelo Location, con sub-documentos

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+/* Añadiendo MongoDB */
+require('./app_server/models/db');
+
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 
