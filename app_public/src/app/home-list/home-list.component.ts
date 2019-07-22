50e671b2de82556c0c78a77a302872d0df3be591
Hector Perez M <hector.perez.m@usach.cl>
2019-07-22 17:04:22 -0400

    añadido un pipe para formatear distancia

--- app_public/src/app/home-list/home-list.component.ts
+++ app_public/src/app/home-list/home-list.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Location {
+  _id: string;
+  name: string;
+  distance: number;
+  address: string;
+  rating: number;
+  facilities: string[];
+}
+
 @Component({
   selector: 'app-home-list',
   templateUrl: './home-list.component.html',
@@ -8,6 +17,33 @@ import { Component, OnInit } from '@angular/core';
 export class HomeListComponent implements OnInit {
 
   constructor() { }
+
+  locations: Location[] = [
+    {
+    _id: '5d349ad98678381d9c094159',
+    name: 'Starcups',
+    distance: 14.0,
+    address: 'High Street, Downtown',
+    rating: 3,
+    facilities: ['Hot drinks', 'Foods', 'Wifi']
+    },
+    {
+      _id: '5d349ad98678381d9c094159',
+      name: 'Coffeee',
+      distance: 140000.0,
+      address: 'High Street, Downtown',
+      rating: 5,
+      facilities: ['Hot drinks', 'Foods', 'Wifi']
+    },
+    {
+      _id: '5d349ad98678381d9c094159',
+      name: 'Starcraft!',
+      distance: 11114.0,
+      address: 'High Street, Downtown',
+      rating: 5,
+      facilities: ['Hot drinks', 'Foods', 'Wifi']
+    }
+  ];
 
   ngOnInit() {
   }
